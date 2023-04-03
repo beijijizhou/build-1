177c2d7d12e4247fde27bfe3b1b79bae11f0b7a3
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2023-04-03 10:32:15 -0400

    port number

--- src/CounterButton.js
+++ src/CounterButton.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 function CountButton() {
   const [count, setCount] = useState(0);
-  const backendUrl = 'https://mapworkshop1.herokuapp.com:8080';
+  const backendUrl = `https://mapworkshop1.herokuapp.com:${process.env.PORT}`;
+  console.log(process.env.PORT)
   async function handleClick () {
     const response = await axios.get(`${backendUrl}/count`);
     console.log(response)
